2ef9a11c1ee3a0ae58c217118236517ba65524e7
Dan Gardner <45269444+dan-processbolt@users.noreply.github.com>
2019-08-15 16:06:29 -0500

    WPF app done

--- omkwpf/MainWindow.xaml.cs
+++ omkwpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OfficeMasterKey;
 
 namespace omkwpf
 {
@@ -29,5 +30,126 @@ namespace omkwpf
         {
             this.Close();
         }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.None;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy | DragDropEffects.Move;
+            }
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                this.Activate();
+
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                UnprotectFiles(files);
+            }
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                Multiselect = true,
+                DefaultExt = ".xlsx",
+                Filter = "Excel and Word Files (*.xslx,*.docx)|*.xlsx;*.docx|Excel Files (*.xslx)|*.xlsx|Word Files (*.docx)|*.docx|*.*|*.*",
+                CheckFileExists = true
+            };
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                try
+                {
+                    UnprotectFiles(dlg.FileNames);
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message, "Error Processing Files", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void UnprotectFiles(string[] files)
+        {
+            foreach (string file in files)
+            {
+                UnprotectFile(file);
+            }
+        }
+
+        private void UnprotectFile(string file)
+        {
+            MasterKey masterKey = new MasterKey();
+
+            try
+            {
+                StatusText.Text += "Removing protection from: " + file;
+
+                if (masterKey.FileIsXlsx(file))
+                {
+                    if (masterKey.XlsxIsProtected(file))
+                    {
+                        string protectedParts = "";
+
+                        if (masterKey.XlsxIsWorkbookProtected(file) && masterKey.XlsxIsWorksheetProtected(file))
+                        {
+                            protectedParts = "[Workbook,Worksheet]";
+                        }
+                        else if (masterKey.XlsxIsWorkbookProtected(file))
+                        {
+                            protectedParts = "[Workbook]";
+                        }
+                        else if (masterKey.XlsxIsWorksheetProtected(file))
+                        {
+                            protectedParts = "[Worksheet]";
+                        }
+
+                        masterKey.UnprotectXlsx(file);
+                        StatusText.Text += "  OK. " + protectedParts + Environment.NewLine;
+                    }
+                    else
+                    {
+                        StatusText.Text += "  Not protected." + Environment.NewLine;
+                    }
+                }
+                else if (masterKey.FileIsDocx(file))
+                {
+                    if (masterKey.DocxIsProtected(file))
+                    {
+                        masterKey.UnprotectDocx(file);
+                        StatusText.Text += "  OK. " + Environment.NewLine;
+                    }
+                    else
+                    {
+                        StatusText.Text += "  Not protected." + Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    StatusText.Text += "  Not recognized as valid DOCX or XLSX file type." + Environment.NewLine;
+                }
+
+            }
+            catch (Exception x)
+            {
+                StatusText.Text += x.Message + Environment.NewLine;
+            }
+        }
+
     }
 }
